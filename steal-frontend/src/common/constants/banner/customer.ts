8a01ceb973936ac96950a91b603f797aeb97fae6
Henry Le <lehieu2077@gmail.com>
2024-04-12 01:39:28 +0700

    feat: add decorator

---
+++ steal-frontend/src/common/constants/banner/customer.ts
@@ -0,0 +1,5 @@
+import NewJeanImage from '@/assets/new_jeans.png';
+import Banner2 from '@/assets/bn2.png';
+import Banner3 from '@/assets/customer_banner_3.png';
+
+export const CUSTOMER_BANNERS = [NewJeanImage, Banner2, Banner3];
--- steal-frontend/src/components/banner/index.tsx
+++ steal-frontend/src/components/banner/index.tsx
@@ -2,6 +2,7 @@
 import { useBannerSlide } from "@/store/hooks";
 import Image, { StaticImageData } from "next/image";
 import { FC, HtmlHTMLAttributes, memo, useState } from "react";
+import shopIcon from '@/assets/shOp.svg';
 
 interface BannerProps extends HtmlHTMLAttributes<HTMLDivElement> {
     src: StaticImageData[];
@@ -16,8 +17,12 @@ const BannerComponent: FC<BannerProps> = (props) => {
         interval
     })
 
-    return <div>
+    return <div className="relative">
         <Image src={src[currentSlideIndex]} alt="Banner image" />
+        <span className="absolute flex flex-col items-start justify-center gap-2 top-[20%] left-[15%]">
+            <Image src={shopIcon} alt="Shop icon" />
+            <span className="text-white text-xl font-bold">We have all you need !</span>
+        </span>
     </div>
 
 }
