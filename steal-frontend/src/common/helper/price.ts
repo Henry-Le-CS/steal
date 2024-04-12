eb5da383765eb45b18d3ad3c04e88eb822ac9bb0
Henry Le <lehieu2077@gmail.com>
2024-04-12 19:30:58 +0700

    dispatch: fix invalid price

--- steal-frontend/src/common/helper/price.ts
+++ steal-frontend/src/common/helper/price.ts
@@ -6,11 +6,11 @@ export const getPriceRange = (unit: PriceUnit, range: number[]) => {
     switch (unit) {
         case PriceUnit.VND:
         default:
-            basePrice = [0, 10000000];
+            basePrice = 10000000;
             break;
     }
 
-    return [basePrice[0]*range[0]/100, basePrice[1]*range[1]/100];
+    return [basePrice*range[0]/100, basePrice*range[1]/100];
 }
 
 export const formartNumber = (number: number) => new Intl.NumberFormat('vi-VN').format(number)
