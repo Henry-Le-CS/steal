4012699f4cff3c5a35131d0b3565b0ea7390e251
Henry Le <lehieu2077@gmail.com>
2024-04-16 02:40:42 +0700

    feat: axios client should be attached with credentials

--- steal-frontend/src/apis/index.ts
+++ steal-frontend/src/apis/index.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export const axiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    withCredentials: true,
 })
 
 export * from './product'
